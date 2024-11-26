94ee0435c48c4188643b7c33177862bdd3f657fb
Sara <so013872@gmail.com>
2024-11-26 22:01:30 +0100

    deklarerar konstanter så att jag kan nå ut till olika element i html dokumentet och ändra deras innehåll

--- index.js
+++ index.js
@@ -101,6 +101,9 @@ function updateBoxDistance(cityObject, kindOfCity) {
     }
 } 
 let cityNamePrompt = prompt ("Stad?")
+const h2 = document.querySelector("h2")
+const h3 = document.querySelector("h3")
+const titlePage = document.querySelector("title")
 }
 
 //markCityBox(cities[0], "target");
