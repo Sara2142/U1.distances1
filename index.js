7fd740f66dd48c21c8035e18049c60700dbe38ed
Sara <so013872@gmail.com>
2024-11-26 19:53:42 +0100

    Funktion som skapar en div (box) åt varje stad + ger varje div ett eget id. Klassen läggs också till så dem får sitt utseende från css

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const mainDiv = document.querySelector("#table")
 const cityNameContainer = document.querySelector("#cities")
+function createAllCityBoxes(cityName, idName){
+    let div = document.createElement("div")
+    cityNameContainer.append(div)
+    div.className = "cityBox"
+    div.id = idName
+    div.textContent = cityName
+}
