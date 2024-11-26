4f522391bdac1865a0ecd25910d586bf6d380c1c
Sara <so013872@gmail.com>
2024-11-26 21:05:18 +0100

    En funktion som används för att få reda på längsta avståndet i relation till vår target. Det mesta är samma förutom här så updateras furthest city och distance baserat på om objektets distans är större än det den jämför med

--- index.js
+++ index.js
@@ -43,7 +43,45 @@ function getClosestCity(targetCityObject) {
     return { city: nearCity, distance: nearDistance };
 const closestCity = getClosestCity(cities[16]);
 console.log("Closest City:", closestCity.city.name, closestCity.distance);
+ function getFurthestCity(targetCityObject) {
+    let furthestCity = null;
+    let furthestDistance = 0;  
+
+ 
+    for (let objects of distances) {
+        let secondCityId;
+
+        if (objects.city1 == targetCityObject.id) {
+            secondCityId = objects.city2;
+        } else if (objects.city2 == targetCityObject.id) {
+            secondCityId = objects.city1;
+        } 
+
+
+        let secondCityName = null;
+        for (let city of cities) {
+            if (city.id == secondCityId) {
+                secondCityName = city;
+                break; 
+            }
+        }
+
+       
+        if (secondCityName) {
+            if (objects.distance > furthestDistance) {
+                furthestCity = secondCityName;
+                furthestDistance = objects.distance;
+            }
+        }
     }
+
+    return { city: furthestCity, distance: furthestDistance };
+    
+}
+
+const furthestCity = getFurthestCity(cities[16]);
+console.log("Furthest City:", furthestCity.city.name, furthestCity.distance);
+
 }
 
 //markCityBox(cities[0], "target");
